0a674ef45082f7198a9e8714e1bf75badf3b8d87
Max Neuvians <max.neuvians@cds-snc.ca>
2022-07-15 13:00:49 +0000

    feat: adds a staging/dev bucket and scanning

--- terraform/file_scanning.tf
+++ terraform/file_scanning.tf
@@ -6,3 +6,12 @@ module "s3_scan_objects" {
 
   billing_tag_value = var.billing_code
 }
+
+module "s3_scan_objects_dev" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.9//S3_scan_object"
+
+  product_name          = "share-files-securely-dev"
+  s3_upload_bucket_name = module.share_files_securely_bucket_dev.s3_bucket_id
+
+  billing_tag_value = var.billing_code
+}
