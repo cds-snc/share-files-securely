cb35beb4f519c657527eff41905d38f6c36f1012
Max Neuvians <max.neuvians@cds-snc.ca>
2022-07-15 13:14:34 +0000

    fix: fmt

--- terraform/file_scanning.tf
+++ terraform/file_scanning.tf
@@ -10,8 +10,8 @@ module "s3_scan_objects" {
 module "s3_scan_objects_dev" {
   source = "github.com/cds-snc/terraform-modules?ref=v3.0.9//S3_scan_object"
 
-  product_name          = "share-files-securely-dev"
-  s3_upload_bucket_name = module.share_files_securely_bucket_dev.s3_bucket_id
+  product_name                  = "share-files-securely-dev"
+  s3_upload_bucket_name         = module.share_files_securely_bucket_dev.s3_bucket_id
   scan_files_assume_role_create = false
 
   billing_tag_value = var.billing_code
