6f0953bd73fd86a2f9920b092e3919102da18db4
Max Neuvians <max.neuvians@cds-snc.ca>
2022-07-15 09:16:07 -0400

    feat: adds a staging/dev bucket and scanning (#7)
    * feat: adds a staging/dev bucket and scanning
    
    * fix: staging scan to false
    
    * fix: fmt

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ _La version française sera disponible bientôt_
 This repository is a proof of concept for a secure file sharing service.
 
 https://user-images.githubusercontent.com/867334/176796040-5b054541-201e-402c-b66d-ff40c07ab40a.mp4
+
+
+## Running in dev container
+
+It is easiest to work with this app in GitHub codespaces as all the env variables a properly pre-populated. Once it is opened in codespaces run: 
+
+```
+cd app
+make init
+make init-dev
+make migrate
+make serve
+```
+
+Make sure you have the temporary AWS credentials for the SRE Tools account otherwise you will not be able to interact with the S3 bucket.
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -27,3 +27,33 @@ resource "aws_s3_bucket_cors_configuration" "share_files_securely_bucket" {
     expose_headers  = []
   }
 }
+
+module "share_files_securely_bucket_dev" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.5//S3"
+  billing_tag_value = var.billing_code
+  bucket_name       = "share-files-securely-dev"
+
+  versioning = {
+    enabled = true
+  }
+
+  lifecycle_rule = [{
+    id      = "expire"
+    enabled = true
+    expiration = {
+      days = 30
+    }
+  }]
+}
+
+resource "aws_s3_bucket_cors_configuration" "share_files_securely_bucket_dev" {
+  bucket = module.share_files_securely_bucket_dev.s3_bucket_id
+
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "DELETE"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+  }
+}
