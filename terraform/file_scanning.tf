29ebd04900bc298c0b6015507f757dc465225422
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-07-10 15:00:09 -0700

    Removing product name as the module has changed

--- terraform/file_scanning.tf
+++ terraform/file_scanning.tf
@@ -1,7 +1,6 @@
 module "s3_scan_objects" {
   source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.1"
 
-  product_name          = "share-files-securely"
   s3_upload_bucket_name = module.share_files_securely_bucket.s3_bucket_id
 
   billing_tag_value = var.billing_code
@@ -10,7 +9,6 @@ module "s3_scan_objects" {
 module "s3_scan_objects_dev" {
   source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.1"
 
-  product_name                  = "share-files-securely-dev"
   s3_upload_bucket_name         = module.share_files_securely_bucket_dev.s3_bucket_id
   scan_files_assume_role_create = false
 
