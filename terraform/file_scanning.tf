8140143aed1ac9d27afa3958de506f4917b1372d
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-07-12 10:17:20 -0700

    Formatting file scanning file

--- terraform/file_scanning.tf
+++ terraform/file_scanning.tf
@@ -1,13 +1,13 @@
 module "s3_scan_objects" {
-  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
+  source                = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
   s3_upload_bucket_name = module.share_files_securely_bucket.s3_bucket_id
-  billing_tag_value = var.billing_code
+  billing_tag_value     = var.billing_code
 }
 
 module "s3_scan_objects_dev" {
-  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
-  scan_queue_suffix = "DEV"
+  source                        = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
+  scan_queue_suffix             = "DEV"
   s3_upload_bucket_name         = module.share_files_securely_bucket_dev.s3_bucket_id
   scan_files_assume_role_create = false
-  billing_tag_value = var.billing_code
+  billing_tag_value             = var.billing_code
 }
