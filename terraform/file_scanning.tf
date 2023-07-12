9b396652e75a1f284ddb3b183b460bd996c31493
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-07-12 10:06:41 -0700

    Update terraform/file_scanning.tf
    Co-authored-by: Pat Heard <patrick.heard@cds-snc.ca>

--- terraform/file_scanning.tf
+++ terraform/file_scanning.tf
@@ -7,7 +7,7 @@ module "s3_scan_objects" {
 }
 
 module "s3_scan_objects_dev" {
-  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.1"
+  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
 
   s3_upload_bucket_name         = module.share_files_securely_bucket_dev.s3_bucket_id
   scan_files_assume_role_create = false
